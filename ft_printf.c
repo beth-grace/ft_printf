8786226645085a7957f5060a936c76f60364d02a
Bethany Milford <bmilford@F3C025.42adel.org.au>
2024-04-24 15:17:53 +0930

    pls work

--- ft_printf.c
+++ ft_printf.c
@@ -6,51 +6,55 @@
 /*   By: bmilford <bmilford@student.42adel.o>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 14:42:23 by bmilford          #+#    #+#             */
-/*   Updated: 2024/04/23 16:48:49 by bmilford         ###   ########.fr       */
+/*   Updated: 2024/04/24 15:16:48 by bmilford         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "ft_printf.h"
 
 int	ft_printf(const char *format, ...)
 {
-	va_list args;
+	va_list	args;
 	int		index;
+	int		out;
 
 	index = 0;
+	out = 0;
 	va_start(args, format);
 	while (format[index])
 	{
 		if (format[index] == '%')
-			checkcase(format[++index], args);
+			out += checkcase(format[++index], args);
 		else
-			write(1, format + index, 1);
+			out += write(1, format + index, 1);
 		index++;
 	}
 	va_end(args);
-	return (1);
+	return (out);
 }
 
-void	checkcase(char cases, va_list args)
+int	checkcase(char cases, va_list args)
 {
 	if (cases == '%')
-		ft_putchar_fd('%', 1);
+		return (ft_putchar_fd('%', 1));
 	else if (cases == 'c')
-		ft_putchar_fd(va_arg(args, int), 1);
+		return (ft_putchar_fd(va_arg(args, int), 1));
 	else if (cases == 's')
-		ft_putstr_fd(va_arg(args, char*), 1);
+		return (ft_putstr_fd(va_arg(args, char *), 1));
 	else if (cases == 'p')
-		ft_putptr_fd(va_arg(args, size_t), 1);
+		return (ft_putptr_fd(va_arg(args, size_t), 1));
 	else if (cases == 'd')
-		ft_putnbr_fd(va_arg(args, int), 1);
+		return (ft_putnbr_fd(va_arg(args, int), 1));
 	else if (cases == 'i')
-		ft_putnbr_fd(va_arg(args, int), 1);
+		return (ft_putnbr_fd(va_arg(args, int), 1));
 	else if (cases == 'u')
-		ft_putunbr_fd(va_arg(args, unsigned int), 1);
+		return (ft_putunbr_fd(va_arg(args, unsigned int), 1));
 	else if (cases == 'x')
-		ft_puthex_fd(va_arg(args, unsigned int),
-			"0123456789abcdef", 1);
+		return (ft_puthex_fd(va_arg(args, unsigned int),
+				"0123456789abcdef", 1));
 	else if (cases == 'X')
-		ft_puthex_fd(va_arg(args, unsigned int),
-			"0123456789ABCDEF", 1);
+		return (ft_puthex_fd(va_arg(args, unsigned int),
+				"0123456789ABCDEF", 1));
+	return (-1);
 }
 
 int	ft_putptr_fd(size_t n, int fd)
@@ -58,6 +62,6 @@ int	ft_putptr_fd(size_t n, int fd)
 	int	t;
 
 	t = ft_putstr_fd("0x", fd);
-	t += ft_puthex_fd(n, "0123456789ABCDEF", fd);
+	t += ft_puthex_fd(n, "0123456789abcdef", fd);
 	return (t);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,13 +6,13 @@
 /*   By: bmilford <bmilford@student.42adel.o>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 16:48:26 by bmilford          #+#    #+#             */
-/*   Updated: 2024/04/23 16:48:53 by bmilford         ###   ########.fr       */
+/*   Updated: 2024/04/24 14:15:38 by bmilford         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include "libft.h"
 
-void	checkcase(char cases, va_list args);
+int		checkcase(char cases, va_list args);
 int		ft_putptr_fd(size_t n, int fd);
 int		ft_printf(const char *format, ...);
